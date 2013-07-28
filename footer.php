511f8708f5f8db52741f3e2038d755628594c27b
Sean Davis <sdavis2702@gmail.com>
2013-07-28 15:14:23 -0500

    add theme customizer copyright content

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * the template for displaying the footer
+ * the template for displaying the closing content area and footer
  */
 ?>
 
@@ -11,12 +11,26 @@
 		<div class="footer-area full">
 			<div class="main">
 				<footer class="site-footer inner">
-					<!-- footer HTML -->
+			
+					<?php
+						$credits = __( 'Built with WordPress & <a href="' . SDM_HOME . '">' . SDM_NAME . '</a>', 'sdm' );
+						
+						// If copyright & credits are left empty or have not been set, display default info.
+						if ( '' == get_theme_mod( 'sdm_credits_copyright' ) ) :
+							echo $credits;
+						else :
+							echo get_theme_mod( 'sdm_credits_copyright' );
+						endif;
+					?>
+					
 				</footer>
 			</div>
 		</div>
 		
-		<?php wp_footer(); ?>
+		<?php 
+			// do not remove WordPress' footer hook
+			wp_footer(); 
+		?>
 	
 	</body>
 </html>
