17ee74c59e079b30414ba4722d4192366b6b2538
Sean Davis <sdavis2702@gmail.com>
2014-04-24 12:42:04 -0500

    add excerpt to downloads on download archive/store

--- templates/content-download-taxonomy.php
+++ templates/content-download-taxonomy.php
@@ -34,6 +34,9 @@
 					<a class="product-title" href="<?php the_permalink(); ?>">
 						<h3><?php the_title(); ?></h3>
 					</a>
+					<div class="product-info">
+						<?php the_excerpt(); ?>
+					</div>
 				</div>
 				<?php if ( get_theme_mod( 'pdt_product_view_details' ) ) : ?>
 					<a class="view-details" href="<?php the_permalink(); ?>"><?php echo get_theme_mod( 'pdt_product_view_details' ); ?></a>
