ad2861923905ee5a3f98c03fc7abaec6c543a7e1
Sean Davis <sdavis2702@gmail.com>
2022-03-01 09:18:22 -0600

    refactoring styles

--- templates/content.php
+++ templates/content.php
@@ -3,21 +3,21 @@
  * This template is the most generic of all content displaying templates.
  * If the content type is not specified, like a single post or page, this
  * template is used to structure the content.
- */ 
+ */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'post-feed' ); ?>>
 	<header class="entry-header">
 		<a class="entry-title" href="<?php the_permalink(); ?>">
-			<?php the_title( '<span class="title-icon"><i class="fa fa-link"></i></span><h1 class="entry-title">', '</h1>' ); ?>
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		</a>
 		<?php if ( 'post' == get_post_type() ) { ?>
 			<div class="entry-meta">
 				<?php pdt_posted_on(); ?>
 			</div>
 		<?php } ?>
-	</header>	
-	<?php $post_content_type = ( 1 == get_theme_mod( 'pdt_post_content' ) ? 'entry-content' : 'entry-summary' ); ?>	
+	</header>
+	<?php $post_content_type = ( 1 == get_theme_mod( 'pdt_post_content' ) ? 'entry-content' : 'entry-summary' ); ?>
 	<section class="<?php echo $post_content_type; ?>">
 		<?php
 			if ( 1 == get_theme_mod( 'pdt_post_content' ) ) {
@@ -26,8 +26,8 @@
 						<?php the_post_thumbnail( 'full', array( 'class' => 'featured-img' ) ); ?>
 					</div>
 					<?php
-				}				
-				the_content( get_theme_mod( 'pdt_read_more', __( 'Read More', 'pdt' ) . ' &rarr;' ) );	
+				}
+				the_content( get_theme_mod( 'pdt_read_more', __( 'Read More', 'pdt' ) . ' &rarr;' ) );
 				wp_link_pages( array(
 					'before' => '<div class="page-links">' . __( 'Pages:', 'pdt' ),
 					'after'  => '</div>',
