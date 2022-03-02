c702b373f76e5df8df08c250647914f5bad4d93c
Sean Davis <sdavis2702@gmail.com>
2022-03-02 07:37:23 -0600

    remove Featured Info feature #3

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ $ping = get_bloginfo( 'pingback_url' );
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		
+
 		<?php // site wide header area ?>
 		<div class="header-area full">
 			<div class="main">
@@ -42,17 +42,17 @@ $ping = get_bloginfo( 'pingback_url' );
 				</header>
 			</div>
 		</div>
-			
+
 		<div class="menu-area full">
 			<div class="main">
 				<nav id="site-navigation" class="main-navigation menu inner clear-pdt" role="navigation">
 					<span class="menu-toggle"><i class="fa fa-bars"></i> <?php _e( 'Menu', 'pdt' ); ?></span>
 					<?php
-						// main menu configuration 
-						$menu_args = array( 
+						// main menu configuration
+						$menu_args = array(
 							'theme_location' => 'main',
 							'container_class' => 'main-menu',
-							'fallback_cb' => 'pdt_menu_fallback' 
+							'fallback_cb' => 'pdt_menu_fallback'
 						);
 						wp_nav_menu( $menu_args );
 					?>
@@ -62,16 +62,7 @@ $ping = get_bloginfo( 'pingback_url' );
 				</nav>
 			</div>
 		</div>
-	
-		<?php // site wide feature box area - leave wrapping HTML in place no matter what for nice design effect ?>
-		<div class="feature-area full">
-			<div class="main">
-				<div class="feature-box inner clear-pdt">
-					<?php get_template_part( 'templates/content', 'feature-box' ); ?>
-				</div>
-			</div>
-		</div>
-		
+
 		<?php // site wide content area... closes in footer.php ?>
 		<div class="content-area full">
 			<div class="main">
